e89705a2aa078cf39f98ce018700b1f0e03490f1
zahra nabilah <zahranabila0508@gmail.com>
2026-05-02 18:39:21 +0700

    praktikum 2.2 not found exceptions

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -35,9 +35,11 @@ class UserController extends Controller
     
         //UserModel::create($data);
 
-        $user = UserModel::findOr(20, ['username', 'nama'], function () {
+       /* $user = UserModel::findOr(20, ['username', 'nama'], function () {
             abort(404);
-        });
+        });*/
+
+        $user = UserModel::where('username', 'manager9')->firstOrFail();
         return view('user', ['data' => $user]);
             
 
