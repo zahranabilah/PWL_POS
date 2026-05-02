1dfdb979b45ef1a7f13fe8b1296481b57931befe
zahra nabilah <zahranabila0508@gmail.com>
2026-05-02 17:19:42 +0700

    praktikum 5 implementasi Query Builder

--- routes/web.php
+++ routes/web.php
@@ -2,9 +2,11 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\LevelController;
+use App\Http\Controllers\KategoriController;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
 Route::get('/level', [LevelController::class, 'index']);
+Route::get('/kategori', [KategoriController::class, 'index']);
