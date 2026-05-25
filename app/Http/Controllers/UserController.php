26c986186e3fd4590442a7355382211c30f79aed
zahra nabilah <zahranabila0508@gmail.com>
2026-05-25 19:18:53 +0700

    Jobsheet(06)-Praktikum3_Modal Ajax Hapus Data (Data User)

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -271,6 +271,37 @@ class UserController extends Controller
     }
     return redirect('/');
 }
+    public function confirm_ajax(string $id)
+    {
+        $user = UserModel::with('level')->find($id);
+
+        return view('user.confirm_ajax', [
+            'user' => $user
+        ]);
+    }
+    public function delete_ajax(Request $request, $id)
+    {
+        if ($request->ajax() || $request->wantsJson()) {
+
+            $user = UserModel::find($id);
+
+            if ($user) {
+                $user->delete();
+
+                return response()->json([
+                    'status' => true,
+                    'message' => 'Data berhasil dihapus'
+                ]);
+            } else {
+                return response()->json([
+                    'status' => false,
+                    'message' => 'Data tidak ditemukan'
+                ]);
+            }
+        }
+
+        return redirect('/');
+    }
 
 }
     /*public function tambah()
